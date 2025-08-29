98f9c6c24e06cc5034155128e6693ac826ac4f84
Jaladhi Patel <jaladhi.dev@gmail.com>
2025-08-29 11:54:46 +0530

    handled any amount of numbers

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -1,5 +1,14 @@
-class StringCalculator{
-  int add(String num){
-    return 0;
+class StringCalculator {
+  int add(String numbers) {
+    //handled the empty string
+    if (numbers.trim().isEmpty) {
+      return 0;
+    }
+
+    //handled any amount of number
+    final splitNumbers = numbers.split(',');
+    final numberList = splitNumbers.map((element) => int.parse(element.trim()));
+    final sum = numberList.fold(0, (previousValue, element) => previousValue + element);
+    return sum;
   }
 }
--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -11,5 +11,12 @@ void main(){
     test('Should return 0 for empty string',(){
       expect(calculator.add(''), 0);
     });
+    
+    test('Should handled any amount of numbers', (){
+      expect(calculator.add('1'), 1);
+      expect(calculator.add('1,2'), 3);
+      expect(calculator.add('1,2,4'), 7);
+      expect(calculator.add('10,20,30'), 60);
+    });
   });
 }
