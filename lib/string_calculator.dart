9f17a875496829923c2f3e3af865f640122061ef
Jaladhi Patel <jaladhi.dev@gmail.com>
2025-08-29 13:02:54 +0530

    Improve structure and added new test cases

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -2,39 +2,56 @@ import 'package:kata_string_calculator/regex_constant.dart';
 
 class StringCalculator {
   int add(String numbers) {
-    //handled the empty string
-    if (numbers.trim().isEmpty) {
-      return 0;
-    }
-    RegExp delimiterRegexp = RegexConstant.commaOrNewLineRegex;
-
-    //check custom delimiters
-    if (numbers.startsWith('//')) {
-      final split = numbers.split('\n');
-      //check the delimiter format
-      if (split.length < 2) {
-        throw FormatException('Invalid custom delimiter');
+    try {
+      //handled the empty string
+      if (numbers
+          .trim()
+          .isEmpty) {
+        return 0;
       }
-      final dynamicDelimiter = split[0].substring(2);
-      delimiterRegexp = RegExp(RegExp.escape(dynamicDelimiter));
-      numbers = split.sublist(1).join('\n');
-
-      //throw exception for duplicate values
-      if (numbers.contains('\n') || numbers.contains(',')) {
-        throw FormatException('Invalid input: mixed delimiters used with custom delimiter');
-      }
+      //check the custom delimiters
+      final customDelimiters = _extractDelimiter(numbers);
+      final delimiterRegex = customDelimiters.$1;
+      final numberString = customDelimiters.$2;
+      //split the numbers with regexp and parse
+      final numbersList = _parseNumbers(numberString, delimiterRegex);
+      //check for negative numbers
+      _checkForNegatives(numbersList);
+      //sum of numbers
+      final sum = numbersList.fold(0, (previousValue, element) => previousValue + element);
+      return sum;
+    } catch (e) {
+      rethrow;
     }
-    //split the numbers with regexp
-    final splitNumbers = numbers.split(delimiterRegexp);
-    //convert to integer
-    final numbersList = splitNumbers.map((element) => int.parse(element.trim()));
-    //check negatives numbers
-    final negatives = numbersList.where((n) => n < 0).toList();
+  }
+
+
+  (RegExp, String) _extractDelimiter(String input) {
+    if (!input.startsWith('//')) {
+      return (RegexConstant.commaOrNewLineRegex, input);
+    }
+
+    final parts = input.split('\n');
+    if (parts.length < 2) {
+      throw FormatException('Invalid custom delimiter format');
+    }
+
+    final customDelimiter = parts[0].substring(2);
+    final numberString = parts.sublist(1).join('\n');
+    final delimiterRegex = RegExp(RegExp.escape(customDelimiter));
+    return (delimiterRegex, numberString);
+  }
+
+
+  List<int> _parseNumbers(String numbers, RegExp delimiterRegex) {
+    final numbersList = numbers.split(delimiterRegex).map((element) => int.parse(element.trim())).toList();
+    return numbersList;
+  }
+
+  void _checkForNegatives(List<int> numbers) {
+    final negatives = numbers.where((n) => n < 0).toList();
     if (negatives.isNotEmpty) {
       throw FormatException('Negative numbers not allowed: ${negatives.join(',')}');
     }
-    //sum of numbers
-    final sum = numbersList.fold(0, (previousValue, element) => previousValue + element);
-    return sum;
   }
 }
--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -1,19 +1,19 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:kata_string_calculator/string_calculator.dart';
 
-void main(){
-  group('StringCalculator',(){
+void main() {
+  group('StringCalculator', () {
     late StringCalculator calculator;
-    setUp((){
+    setUp(() {
       calculator = StringCalculator();
     });
 
-    test('Should return 0 for empty string',(){
+    test('Should return 0 for empty string', () {
       expect(calculator.add(''), 0);
       expect(calculator.add('   '), 0);
     });
-    
-    test('Should handled any amount of numbers', (){
+
+    test('Should handled any amount of numbers', () {
       expect(calculator.add('1'), 1);
       expect(calculator.add('1,2'), 3);
       expect(calculator.add('1,2,4'), 7);
@@ -21,7 +21,7 @@ void main(){
       expect(calculator.add('1 , 2 , 3 '), 6);
     });
 
-    test('Should handled new line between numbers',(){
+    test('Should handled new line between numbers', () {
       expect(calculator.add('1\n2'), 3);
       expect(calculator.add('1\n2,7'), 10);
       expect(calculator.add('1\n2\n3\n4\n5'), 15);
@@ -29,24 +29,14 @@ void main(){
       expect(calculator.add('//*\n1*2*5'), 8);
       expect(calculator.add('//+\n10+30+50'), 90);
     });
-    
-    test('Should handled custom delimiter', (){
+
+    test('Should handled custom delimiter', () {
       expect(calculator.add('//;\n1;2'), 3);
       expect(calculator.add('//&\n1&2&9'), 12);
       expect(calculator.add('//*\n10*20*40'), 70);
     });
 
-    test('Should throw FormatException for a single negative number', () {
-      try {
-        calculator.add('1,-2,3');
-        fail('Expected FormatException');
-      } catch (e) {
-        expect(e, isA<FormatException>());
-        expect(e.toString(), contains('Negative numbers not allowed: -2'));
-      }
-    });
-
-    test('Should throw FormatException for multiple negative numbers', () {
+    test('Should throw FormatException for negative numbers', () {
       try {
         calculator.add('-1,2,-3,4');
         fail('Expected FormatException');
@@ -54,6 +44,32 @@ void main(){
         expect(e, isA<FormatException>());
         expect(e.toString(), contains('Negative numbers not allowed: -1,-3'));
       }
+    });
+
+    test('Should throw FormatException if custom delimiter is incorrect', () {
+      expect(() => calculator.add('//;\n'), throwsFormatException);
+      expect(() => calculator.add('//;\n\n'), throwsFormatException);
+      expect(() => calculator.add('//'), throwsFormatException);
+    });
+
+    test('Should throw FormatException for negative numbers with custom delimiter', () {
+      try {
+        calculator.add('//;\n1;-2;3;-5');
+        fail('Expected FormatException');
+      } catch (e) {
+        expect(e, isA<FormatException>());
+        expect(e.toString(), contains('Negative numbers not allowed: -2,-5'));
+      }
+    });
+
+    test('Should throw FormatException for invalid number format', () {
+      expect(() => calculator.add('1,a,3'), throwsFormatException);
+      expect(() => calculator.add('//;\n1;abc;3'), throwsFormatException);
+    });
+
+    test('Should throw FormatException when input starts with // but is not followed by delimiter and newline', () {
+      expect(() => calculator.add('//;\n'), throwsFormatException);
+      expect(() => calculator.add('//\n1;2'), throwsFormatException);
     });
   });
 }
